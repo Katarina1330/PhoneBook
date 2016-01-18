1f94ce1bcf9c8d7b52a38a475384edebe94ae21a
Katarina1330 <Katarina@Katarinas-MacBook-Pro.local>
2016-01-17 22:33:28 -0800

    US-1 #closed

--- src/PhoneBookUI/Program.java
+++ src/PhoneBookUI/Program.java
@@ -5,7 +5,7 @@ import org.eclipse.swt.widgets.*;
 public class Program {
 
 	public static void main(String[] args) {
-		// Ovo je SWT code:
+		
 		Display display = new Display();
 		Shell shell = new Shell();
 		shell.setText("Phone Book");
